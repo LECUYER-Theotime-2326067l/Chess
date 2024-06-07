41edda483d6a359aafc762ee7ec62041e81db66a
Théotime Lecuyer <theotime.lecuyer@etu.univ-amu.fr>
2024-06-07 12:11:24 +0200

    Retrait d'imports inutiles

--- src/main/java/chess/view/MenuButtonView.java
+++ src/main/java/chess/view/MenuButtonView.java
@@ -7,7 +7,6 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
 public class MenuButtonView extends Button {
