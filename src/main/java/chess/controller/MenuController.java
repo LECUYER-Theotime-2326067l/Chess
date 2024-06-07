e16c28fa3554ca9df92f3a86d1e87f7d5c87e7d0
Théotime Lecuyer <theotime.lecuyer@etu.univ-amu.fr>
2024-06-07 12:08:32 +0200

    Refactor du nom des méthodes du contrôlleur de menus

--- src/main/java/chess/controller/MenuController.java
+++ src/main/java/chess/controller/MenuController.java
@@ -5,7 +5,12 @@ import chess.view.StartingView;
 
 public class MenuController {
 
-    public static void startVersusGame() {
+    public static void openVersusMenu() {
+        App.removeMenu();
+        App.addMenu(new StartingView());
+    }
+
+    public static void openComputerMenu() {
         App.removeMenu();
         App.addMenu(new StartingView());
     }
